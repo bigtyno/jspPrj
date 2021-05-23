601741d0c63d159f2f38a7ff8e9f1005d5b9440d
mckmoon <38914221+mckmoon@users.noreply.github.com>
2021-05-23 23:43:12 +0900

    jsp공부

---
+++ JSPprj/src/main/java/com/newlecture/web/Nana.java
@@ -0,0 +1,30 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Nana extends HttpServlet {
+	 
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+		PrintWriter out =response.getWriter();
+		
+		String cnt_ = request.getParameter("cnt");
+		
+		int cnt = 100;
+		if(cnt_ !=null && !cnt_.equals(""))
+			cnt = Integer.parseInt(cnt_);
+		
+		for(int i=0;  i<cnt; i++) 
+		out.println(" Hello world<br>");
+	}
+
+}
